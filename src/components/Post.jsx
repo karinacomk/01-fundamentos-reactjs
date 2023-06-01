11c4654b486b235f06a72a9754a64e4ee3bd086b
Karina De Almeida Lima <karina.lima@mercadolivre.com>
2023-06-01 19:35:41 -0300

    iterando jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,39 +2,9 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css'
 
-const posts = [
-  {
-    id: 1,
-    author: {
-      avatarUrl: 'https://github.com/diego3g.png',
-      name: 'Diego Fernandes',
-      role: 'CTO @Rocketseat'
-    },
-    // se viesse do backend, poderia ser um markdown, não é recomendado html vir do backend por conta de vulnerabilidades, podem executar um script dentro do site
-    content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' },
-    ],
-    publishedAt: new Date('2022-05-03 20:00:00'),
-  },
-  {
-    id: 2,
-    author: {
-      avatarUrl: 'https://github.com/maykbrito.png',
-      name: 'Mayk Brito',
-      role: 'Educator @Rocketseat'
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' },
-    ],
-    publishedAt: new Date('2022-05-10 20:00:00'),
-  },
-];
-
-export function Post() {
+export function Post(props) {
+  console.log(props);
+  
   return (
     <article className={styles.post}>
       <header>
